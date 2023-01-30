7f0f8c0f35af7ed788308782140b5af5efb9eae7
Kayson <282146238@qq.com>
2023-01-30 17:04:57 +0800

    fix imageview.contentMode = UIViewContentModeScaleAspectFill

--- KKBannerView/KKBanner/KKBannerView.m
+++ KKBannerView/KKBanner/KKBannerView.m
@@ -52,7 +52,7 @@
     //添加循环3张图片
     for (int i=0; i<3; i++) {
         UIImageView *img = [UIImageView new];
-        img.contentMode = UIViewContentModeScaleAspectFit;
+        img.contentMode = UIViewContentModeScaleAspectFill;
         img.userInteractionEnabled = YES;
         [img addGestureRecognizer:[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tap:)]];
         [self.imagesArray addObject:img];
